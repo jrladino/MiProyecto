09982d7f834fa0d585a8ceddfd85ce0e9d97bf1e
Jose Rafael Ladino.SONE <jose.ladino@softwareone.com>
2025-04-26 07:50:19 -0500

    segunda parte sesion cuatro

---
+++ src/Condicionales/CicloForEach.java
@@ -0,0 +1,17 @@
+package Condicionales;
+
+public class CicloForEach {
+
+
+    public void printNames(String[] names) {
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+
+    public void printNumbers(int[] numbers) {
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
