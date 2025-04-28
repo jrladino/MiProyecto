a71d8723930060f8a0b7cf776d530b88c56c9190
Jose Rafael Ladino.SONE <jose.ladino@softwareone.com>
2025-04-27 21:28:22 -0500

    Update interest rate calculation in Bancolombia class to 6.5%

--- src/Polimorfismo/Bancolombia.java
+++ src/Polimorfismo/Bancolombia.java
@@ -3,7 +3,7 @@ package Polimorfismo;
 public class Bancolombia extends Banco {
     @Override
     public int getInteres(int monto) {
-        return (int) (monto * 0.06); // Interés del 6%
+        return (int) (monto * 0.065); // Interés del 6.5%
     }
     public String getNombreBanco() {
         return "Bancolombia";
